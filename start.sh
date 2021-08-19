e407d4466232eebbc35c952bbba254cae4838386
chittaranjan2021 <83960157+chittaranjan2021@users.noreply.github.com>
2021-08-19 23:29:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3 /RadioPlayerV3
+git clone https://github.com/chittaranjan2021/RadioPlayerV3 /RadioPlayerV3
 cd /RadioPlayerV3
 echo "Starting Bot, Please Wait..."
 python3 main.py
